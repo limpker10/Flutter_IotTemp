f5bba785db6f3d1c9daeed31014044b0d9b35e7c
Jose Cano <jcanov@unsa.edu.pe>
2023-10-08 11:02:24 -0500

    git init proyect

---
+++ android/app/src/main/kotlin/pe/edu/unsa/iot_temp_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package pe.edu.unsa.iot_temp_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+ThemeData lighTheme = ThemeData(
+    brightness: Brightness.light
+);
